c692a24f76ad3a8e462c2d2028e54961eeb33d7e
huoxudong125 <423huo@163.com>
2017-09-29 09:10:18 +0800

    Fix Blog Components Demo

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -2,9 +2,14 @@
   <div id="vue-app">
     <h2>Blog Detail</h2>
     <div v-for="blog in $data.blogs">
-      <!--<post :post="blog.post"></post>-->
-      <!--<comments :comments="blog.post.comments"></comments>-->
-      <h3>{{blog.name}}</h3>
+
+      <h3>Blog is :{{blog.name}}</h3>
+
+      <h4>Post:</h4>
+      <post :post="blog.post"></post>
+      <h4>Commnents :</h4>
+      <comments :comments="blog.post.comments"></comments>
+
     </div>
   </div>
 </template>
@@ -19,36 +24,51 @@
           title: 'Hello World!',
           body: 'This is a blog content.',
           comments: [{
-            comment: 'good demo',
+            body: 'good demo',
           },
           ],
         },
       },
     ],
   };
+
+
   export default {
     name: 'blog',
 
     data() {
       return initBlogs;
     },
+    components: {
+      post: {
+        // Obviously your template should probably be a bit more than this...
+        template: '<div><h1>{{post.title}}</h1><p>{{post.body}}</p></div>',
+
+        // Data bindings from parent
+        props: ['post'],
+      },
+      comments: {
+        template: '<ul><li v-for="comment in comments">{{ comment.body }}</li></ul>',
+        props: ['comments'],
+      },
+    },
   };
 
-//  Vue.extend({
-//    components: {
-//      post: {
-//        // Obviously your template should probably be a bit more than this...
-//        template: '<h1>{{post.title}}</h1><article>{{post.body}}</article>',
-//
-//        // Data bindings from parent
-//        props: ['post'],
-//      },
-//      comments: {
-//        template: '<ul><li v-for="comment in comments">{{ comment.body }}</li></ul>',
-//        props: ['comments'],
-//      },
-//    },
-//  });
+  // Vue.extend({
+  //    components: {
+  //      post: {
+  //        // Obviously your template should probably be a bit more than this...
+  //        template: '<h1>{{post.title}}</h1><article>{{post.body}}</article>',
+  //
+  //        // Data bindings from parent
+  //        props: ['post'],
+  //      },
+  //      comments: {
+  //        template: '<ul><li v-for="comment in comments">{{ comment.body }}</li></ul>',
+  //        props: ['comments'],
+  //      },
+  //    },
+  //  });
 
   //  ready: function( ) {
   //    var self = this;
