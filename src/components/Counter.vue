84660b2295377a23600776e48d1529cf00209f29
huoxudong125 <423huo@163.com>
2017-10-08 00:54:07 +0800

    add Counter using Vuex modules by mapstates

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -5,18 +5,34 @@
     <button @click="decrement">-</button>
     <button @click="incrementIfOdd">Increment if odd</button>
     <button @click="incrementAsync">Increment async</button>
-    <h2>Current Count is : [{{count}}]</h2>
+    <h2>Current Count is : [{{count}}] by `mapGetters`</h2>
+    <h2>Current Count is : [{{countAlias}}] by `mapState`</h2>
   </div>
 </template>
 
 <script>
-  import { mapGetters, mapActions } from 'vuex';
+  import { mapGetters, mapActions, mapState } from 'vuex';
 
   export default {
-    computed: mapGetters([
-      'evenOrOdd',
-      'count',
-    ]),
+    computed: {
+      ...mapState({
+        // 箭头函数可使代码更简练
+        // count: state => state.count,
+
+        // 传字符串参数 'count' 等同于 `state => state.count`
+        countAlias: 'count',
+
+        // 为了能够使用 `this` 获取局部状态，必须使用常规函数
+//        countPlusLocalState(state) {
+//          return state.count + this.localCount;
+//        },
+      }),
+      // 使用对象展开运算符将 getter 混入 computed 对象中
+      ...mapGetters([
+        'evenOrOdd',
+        'count',
+      ]),
+    },
     methods: mapActions([
       'increment',
       'decrement',
