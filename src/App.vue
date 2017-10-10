fdeec5641d28f738c15b2e99745329ea3143c60f
huoxudong125 <423huo@163.com>
2017-10-10 14:50:35 +0800

    Add route transition

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,50 @@
 <template>
   <div id="app">
     <!--<img src="./assets/logo.png">-->
-    <router-view></router-view>
+    <!-- 使用动态的 transition name -->
+    <!--<transition :name="transitionName">-->
+    <transition v-bind:name="transitionName">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-};
+
+  export default {
+    name: 'app',
+    data() {
+      return { transitionName: 'fade' };
+    },
+    // 接着在父组件内
+    // watch $route 决定使用哪种过渡
+    watch: {
+      $route(to, from) {
+        const toDepth = to.path.split('/').length;
+        const fromDepth = from.path.split('/').length;
+        this.transitionName = toDepth < fromDepth ? 'slide-right' : 'fade';
+      },
+    },
+  };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s
+  }
+
+  .fade-enter, .fade-leave-to /* .fade-leave-active in below version 2.1.8 */
+  {
+    opacity: 0
+  }
+
 </style>
